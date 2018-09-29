e04b6b1cb2d16d80472270447a00993ec0cf2b37
D474th3D357R0Y3R <jcazares117@gmail.com>
2018-09-29 12:18:59 -0500

    code edit test for heroku 3

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-// import React from 'react';
-// import Root from '../src/components/Root.js';
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'LiveConnect' });
 });
 
 module.exports = router;
-
-// ReactDOM.render( <Root /> , document.getElementById('root'));
-// registerServiceWorker();
