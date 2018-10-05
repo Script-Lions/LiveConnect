d09a44f3bd6f961d6eeb6111f502701f0dee7d37
mike <angelrosillo2018@gmail.com>
2018-10-05 15:35:21 -0500

    atemp to create collections

--- bin/Database.js
+++ bin/Database.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 //this is how we connect to database to our app
 const URI = "mongodb://localhost/mongodatabase"
-mongoose.connect(URI, { useNewUrlParser: true })
+mongoose.connect(URI)
    .then(db => console.log("DB is connected"))
    .catch(err => console.error(err));
+   
 
 
 module.exports = mongoose;
