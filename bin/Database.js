8da71483ff5d46be39ecea6a24b012a211e592d0
mike <angelrosillo2018@gmail.com>
2018-10-06 12:19:06 -0500

    creatin collection

--- bin/Database.js
+++ bin/Database.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 //this is how we connect to database to our app
 const URI = "mongodb://localhost/mongodatabase"
-mongoose.connect(URI)
+mongoose.connect(URI, { useNewUrlParser: true })
    .then(db => console.log("DB is connected"))
    .catch(err => console.error(err));
-   
+
 
 
 module.exports = mongoose;
